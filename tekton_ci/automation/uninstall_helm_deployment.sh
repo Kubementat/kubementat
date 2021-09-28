415e29b4190225024f860dec74c28537639f075a
Julian Weber <julianweberdev@gmail.com>
2021-09-28 16:39:57 +0200

    tekton automation - refactor helm uninstall of components into a simple script

---
+++ tekton_ci/automation/uninstall_helm_deployment.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+######################################
+#
+# This script removes the given component from the cluster in ENVIRONMENT
+#
+######################################
+
+set -e
+
+ENVIRONMENT="$1"
+COMPONENT_NAME="$2"
+
+if [[ "$ENVIRONMENT" == "" || "$COMPONENT_NAME" == "" ]]; then
+  echo "Usage: uninstall_helm_deployment.sh <ENVIRONMENT_NAME> <COMPONENT_NAME_UPPERCASE>"
+  echo "e.g.: uninstall_helm_deployment.sh dev POLARIS"
+  exit 1
+fi
+
+set -u
+
+DEPLOYMENT_NAMESPACE_VARIABLE_NAME="$(printf '%s' "${COMPONENT_NAME}_DEPLOYMENT_NAMESPACE")"
+echo "DEPLOYMENT_NAMESPACE_VARIABLE_NAME: $DEPLOYMENT_NAMESPACE_VARIABLE_NAME"
+DEPLOYMENT_NAME_VARIABLE_NAME="$(printf '%s' "${COMPONENT_NAME}_DEPLOYMENT_NAME")"
+echo "DEPLOYMENT_NAME_VARIABLE_NAME: $DEPLOYMENT_NAME_VARIABLE_NAME"
+
+echo "#########################"
+echo "Loading configuration from platform_config ..."
+DEPLOYMENT_NAMESPACE="$(jq -r ".${DEPLOYMENT_NAMESPACE_VARIABLE_NAME}" ../../platform_config/"${ENVIRONMENT}"/static.json)"
+DEPLOYMENT_NAME="$(jq -r ".${DEPLOYMENT_NAME_VARIABLE_NAME}" ../../platform_config/"${ENVIRONMENT}"/static.json)"
+
+echo "ENVIRONMENT: $ENVIRONMENT"
+echo ""
+echo "DEPLOYMENT_NAMESPACE: $DEPLOYMENT_NAMESPACE"
+echo "DEPLOYMENT_NAME: $DEPLOYMENT_NAME"
+echo ""
+echo "#########################"
+echo "Helm version:"
+helm version
+echo "#########################"
+
+echo "Uninstalling $COMPONENT_NAME ..."
+helm -n "${DEPLOYMENT_NAMESPACE}" delete "${DEPLOYMENT_NAME}" ||true
+
+kubectl get all -n "${DEPLOYMENT_NAMESPACE}"
+
+echo "If you also want to remove collected data ensure to also delete the pv and pvc"
+echo "You can do this via: kubectl -n ${DEPLOYMENT_NAMESPACE} delete pvc <PVC_NAME>"
+echo "PVC:"
+kubectl get pvc -n "${DEPLOYMENT_NAMESPACE}" | grep "${DEPLOYMENT_NAME}"
+echo "PV:"
+kubectl get pv | grep "${DEPLOYMENT_NAME}"
--- tekton_ci/automation/uninstall_polaris.sh
+++ tekton_ci/automation/uninstall_polaris.sh
@@ -6,40 +6,4 @@
 #
 ######################################
 
-set -e
-
-ENVIRONMENT="$1"
-if [[ "$ENVIRONMENT" == "" ]]; then
-  echo "Usage: uninstall_polaris.sh <ENVIRONMENT_NAME>"
-  echo "e.g.: uninstall_polaris.sh dev"
-  exit 1
-fi
-
-set -u
-
-echo "#########################"
-echo "Loading configuration from platform_config ..."
-POLARIS_DEPLOYMENT_NAMESPACE="$(jq -r '.POLARIS_DEPLOYMENT_NAMESPACE' ../../platform_config/"${ENVIRONMENT}"/static.json)"
-POLARIS_DEPLOYMENT_NAME="$(jq -r '.POLARIS_DEPLOYMENT_NAME' ../../platform_config/"${ENVIRONMENT}"/static.json)"
-
-echo "ENVIRONMENT: $ENVIRONMENT"
-echo ""
-echo "POLARIS_DEPLOYMENT_NAMESPACE: $POLARIS_DEPLOYMENT_NAMESPACE"
-echo "POLARIS_DEPLOYMENT_NAME: $POLARIS_DEPLOYMENT_NAME"
-echo ""
-echo "#########################"
-echo "Helm version:"
-helm version
-echo "#########################"
-
-echo "Uninstalling polaris ..."
-helm -n "${POLARIS_DEPLOYMENT_NAMESPACE}" delete "${POLARIS_DEPLOYMENT_NAME}" ||true
-
-kubectl get all -n "${POLARIS_DEPLOYMENT_NAMESPACE}"
-
-echo "If you also want to remove collected data ensure to also delete the pv and pvc"
-echo "You can do this via: kubectl -n ${POLARIS_DEPLOYMENT_NAMESPACE} delete pvc <PVC_NAME>"
-echo "PVC:"
-kubectl get pvc -n "${POLARIS_DEPLOYMENT_NAMESPACE}" | grep "${POLARIS_DEPLOYMENT_NAME}"
-echo "PV:"
-kubectl get pv | grep "${POLARIS_DEPLOYMENT_NAME}"
+./uninstall_helm_deployment.sh "${ENVIRONMENT}" "POLARIS"
