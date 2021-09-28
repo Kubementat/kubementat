c9875794dfa75305352a118a4ddbb3740044fea2
Julian Weber <julianweberdev@gmail.com>
2021-09-28 10:52:58 +0200

    polaris - fix polaris tunneling and add example for arm64 k8s clusters

--- utilities/open_polaris_tunnel.sh
+++ utilities/open_polaris_tunnel.sh
@@ -25,9 +25,9 @@ echo "ENVIRONMENT: $ENVIRONMENT"
 echo "POLARIS_DEPLOYMENT_NAMESPACE: $POLARIS_DEPLOYMENT_NAMESPACE"
 echo "#########################"
 
-LOCAL_PORT="8200"
+LOCAL_PORT="8080"
 ADDRESS="0.0.0.0"
 pod_name="$(kubectl -n "$POLARIS_DEPLOYMENT_NAMESPACE" get pod -l "app.kubernetes.io/name=polaris,app.kubernetes.io/instance=polaris" -o json | jq -r '.items[0].metadata.name')"
 echo "Pod Name: $pod_name"
 echo "Visit: http://localhost:$LOCAL_PORT"
-kubectl -n "$POLARIS_DEPLOYMENT_NAMESPACE" port-forward --address $ADDRESS "$pod_name" $LOCAL_PORT:8200
+kubectl -n "$POLARIS_DEPLOYMENT_NAMESPACE" port-forward --address $ADDRESS "$pod_name" $LOCAL_PORT:8080
