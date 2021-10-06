6d6fe75a2fbe93634a2d7bebe255d033eaabcfb6
Julian Weber <julianweberdev@gmail.com>
2021-10-06 12:43:12 +0200

    initial setup - add more output to install_kubementat

--- install_kubementat.sh
+++ install_kubementat.sh
@@ -46,18 +46,52 @@ function check_cluster_and_access(){
   echo ""
 }
 
+function print_configuring_section() {
+  component="$1"
+  echo ""
+  echo "######################################################"
+  echo "CONFIGURING: $component"
+  echo "######################################################"
+  echo ""
+}
+
 check_cluster_and_access
 
 pushd tekton_ci/automation > /dev/null
 
+print_configuring_section "Tekton CI"
 ./install_tekton.sh "${ENVIRONMENT}"
+
+print_configuring_section "Loki (Log Aggregator)"
 ./install_logging.sh "${ENVIRONMENT}"
+
+print_configuring_section "Monitoring (Prometheus, Grafana)"
 ./install_monitoring.sh "${ENVIRONMENT}"
+
+print_configuring_section "Tekton Pipelines for TEAM ${TEAM}"
 ./setup_pipelines.sh "${ENVIRONMENT}" "${TEAM}"
+
+print_configuring_section "Tekton Pipeline Triggers for TEAM ${TEAM}"
 ./setup_triggers.sh "${ENVIRONMENT}" "${TEAM}"
+
+print_configuring_section "Vault"
 ./install_vault.sh "${ENVIRONMENT}"
+
+print_configuring_section "Linkerd service mesh"
 ./install_linkerd.sh "${ENVIRONMENT}"
 
 popd > /dev/null
 
-echo "Installed kubementat to cluster successfully"
+echo "Installed kubementat to cluster successfully :D"
+echo ""
+echo "Now you are ready to start making it your own."
+echo ""
+echo "You can run your first hello world pipeline right now:"
+echo "pushd tekton_ci/automation"
+echo "./run_pipeline.sh dev dev1 ../pipeline-runs/hello-world-pipeline-run.yml"
+echo ""
+echo "And view the results via the tekton dashboard:"
+echo "popd"
+echo "pushd utilities"
+echo "Once this command is executed you can visit http://127.0.0.1:9097 in your browser"
+echo "./open_dashboard_tunnel.sh dev"
