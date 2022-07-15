7f832a6d3be6064749a0177040b8765e65a3693e
Julian Weber <julianweberdev@gmail.com>
2022-07-15 11:59:54 +0200

    add prometheus helm exporter installation automation

--- tekton_ci/automation/components/uninstall_monitoring.sh
+++ tekton_ci/automation/components/uninstall_monitoring.sh
@@ -11,3 +11,4 @@ set -e
 ./uninstall_helm_deployment.sh "$1" "PROMETHEUS"
 ./uninstall_helm_deployment.sh "$1" "GRAFANA"
 ./uninstall_helm_deployment.sh "$1" "PROMETHEUS_BLACKBOX_EXPORTER"
+./uninstall_helm_deployment.sh "$1" "PROMETHEUS_HELM_EXPORTER"
