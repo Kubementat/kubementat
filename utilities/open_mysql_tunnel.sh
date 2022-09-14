a44c01f6332a62edf45b075bb45ed8932ee5beac
Julian Weber <julianweberdev@gmail.com>
2022-09-14 12:29:12 +0200

    add utilities/open_mysql_tunnel.sh

---
+++ utilities/open_mysql_tunnel.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#################################
+#
+# Opens a tunnel connection to a pod running mysql within the kubernetes cluster to allow
+# access from the local machine
+#
+#################################
+
+set -e
+
+NAMESPACE="$1"
+POD_NAME="$2"
+PORT="3306"
+if [[ "$3" != "" ]]; then
+  PORT="$3"
+fi
+
+if [[ "$NAMESPACE" == "" || "$POD_NAME" == "" ]]; then
+  echo "Usage: open_mysql_tunnel.sh <NAMESPACE> <POD_NAME> <optional: PORT>"
+  echo "e.g.: open_mysql_tunnel.sh dev1 mysql-0 3306"
+  exit 1
+fi
+
+set -u
+
+echo "#########################"
+echo "NAMESPACE: $NAMESPACE"
+echo "POD_NAME: $POD_NAME"
+echo "PORT: $PORT"
+echo "#########################"
+
+echo "Open another shell and connect via:"
+echo  "mysql -h 127.0.0.1 -P $PORT -u <DATABASE_USERNAME> -p <DATABASE_NAME>"
+echo "######"
+echo "Opening connection ..."
+kubectl -n "$NAMESPACE" port-forward "pod/${POD_NAME}" "$PORT:$PORT"
