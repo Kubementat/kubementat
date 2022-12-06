d89a001498c38c29a7bcdd04900ba1a4bd538fe1
Julian Weber <julianweberdev@gmail.com>
2022-12-06 15:38:22 +0100

    tekton - setup_triggers - change trigger resources - now created within the -pipelines namespace to separate this properly from the tekton standard installation resources, makes this a team and not environment based configuration

--- tekton_ci/automation/setup_triggers.sh
+++ tekton_ci/automation/setup_triggers.sh
@@ -20,11 +20,9 @@ set -u
 
 echo "#########################"
 echo "Loading configuration from platform_config ..."
-TEKTON_NAMESPACE="tekton-pipelines"
 PIPELINE_NAMESPACE="$(jq -r '.PIPELINE_NAMESPACE' ../../platform_config/"${ENVIRONMENT}"/"${TEAM}"/static.json)"
 
 echo "ENVIRONMENT: $ENVIRONMENT"
-echo "TEKTON_NAMESPACE: $TEKTON_NAMESPACE"
 echo "PIPELINE_NAMESPACE: $PIPELINE_NAMESPACE"
 echo "#########################"
 echo
@@ -32,20 +30,26 @@ echo
 echo "Setting up roles for tekton triggers ..."
 
 
-echo "Setting tekton-triggers-sa ..."
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+# service account for controlling tekton triggers
+echo "Setting tekton-triggers-sa in namespace: $PIPELINE_NAMESPACE ..."
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: v1
 kind: ServiceAccount
 metadata:
   name: tekton-triggers-sa
+  labels:
+    managed-by: kubementat
 EOF
 
-echo "Setting up tekton-triggers-role in namespace $TEKTON_NAMESPACE ..."
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+# role granting access to all resources needed to process triggers
+echo "Setting up tekton-triggers-role in namespace $PIPELINE_NAMESPACE ..."
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: Role
 metadata:
   name: tekton-triggers-role
+  labels:
+    managed-by: kubementat
 rules:
 # EventListeners need to be able to fetch all namespaced resources
 - apiGroups: ["triggers.tekton.dev"]
@@ -59,22 +63,29 @@ rules:
 - apiGroups: ["tekton.dev"]
   resources: ["pipelineruns", "pipelineresources", "taskruns"]
   verbs: ["create"]
+# Impersonate Service Accounts
 - apiGroups: [""]
   resources: ["serviceaccounts"]
   verbs: ["impersonate"]
+# Use pod security policies
 - apiGroups: ["policy"]
   resources: ["podsecuritypolicies"]
   resourceNames: ["tekton-triggers"]
   verbs: ["use"]
+- apiGroups: [""]
+  resources: ["events"]
+  verbs: ["create", "patch"]
 EOF
 
-
-echo "Binding tekton-triggers-sa to tekton-triggers-role in namespace $TEKTON_NAMESPACE ..."
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+# bind created role tekton-triggers-role to created service account tekton-triggers-sa
+echo "Binding tekton-triggers-sa to tekton-triggers-role in namespace $PIPELINE_NAMESPACE ..."
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
 metadata:
   name: tekton-triggers-sa-to-role-binding
+  labels:
+    managed-by: kubementat
 subjects:
 - kind: ServiceAccount
   name: tekton-triggers-sa
@@ -84,48 +95,14 @@ roleRef:
   name: tekton-triggers-role
 EOF
 
-echo "Setting up tekton-triggers-pipeline-namespace-role in namespace $PIPELINE_NAMESPACE ..."
-kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
-apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
-metadata:
-  name: tekton-triggers-pipeline-namespace-role
-rules:
-# Permissions to create resources in associated TriggerTemplates
-- apiGroups: ["tekton.dev"]
-  resources: ["pipelineruns", "pipelineresources", "taskruns"]
-  verbs: ["create"]
-- apiGroups: [""]
-  resources: ["serviceaccounts"]
-  verbs: ["impersonate"]
-- apiGroups: ["policy"]
-  resources: ["podsecuritypolicies"]
-  resourceNames: ["tekton-triggers"]
-  verbs: ["use"]
-EOF
-
-echo "Binding tekton-triggers-sa to tekton-triggers-pipeline-namespace-role in namespace $PIPELINE_NAMESPACE ..."
-kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: tekton-triggers-sa-to-pipeline-namespace-role-binding
-subjects:
-- kind: ServiceAccount
-  name: tekton-triggers-sa
-  namespace: $TEKTON_NAMESPACE
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: Role
-  name: tekton-triggers-pipeline-namespace-role
-EOF
-
 echo "Setting up tekton-triggers-bindings-and-interceptors-clusterrole ..."
 kubectl apply -f - <<EOF
 kind: ClusterRole
 apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: tekton-triggers-bindings-and-interceptors-clusterrole
+  labels:
+    managed-by: kubementat
 rules:
   # EventListeners need to be able to fetch any clustertriggerbindings
 - apiGroups: ["triggers.tekton.dev"]
@@ -133,29 +110,33 @@ rules:
   verbs: ["get", "list", "watch"]
 EOF
 
-echo "Binding tekton-triggers-sa to tekton-triggers-bindings-and-interceptors-clusterrole in namespace $TEKTON_NAMESPACE ..."
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+echo "Binding tekton-triggers-sa to tekton-triggers-bindings-and-interceptors-clusterrole in namespace $PIPELINE_NAMESPACE ..."
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
   name: tekton-triggers-sa-to-tekton-triggers-bindings-and-interceptors-clusterrole-binding
+  labels:
+    managed-by: kubementat
 subjects:
 - kind: ServiceAccount
   name: tekton-triggers-sa
-  namespace: $TEKTON_NAMESPACE
+  namespace: $PIPELINE_NAMESPACE
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
   name: tekton-triggers-bindings-and-interceptors-clusterrole
 EOF
 
-echo "Setting up tekton-triggers-createwebhook-role in $TEKTON_NAMESPACE ..."
+echo "Setting up tekton-triggers-createwebhook-role in $PIPELINE_NAMESPACE ..."
 # create create-webhook-rolebinding
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: Role
 metadata:
   name: tekton-triggers-createwebhook-role
+  labels:
+    managed-by: kubementat
 rules:
 - apiGroups:
   - ""
@@ -189,20 +170,24 @@ rules:
   - update
 EOF
 
-echo "Setting up tekton-triggers-createwebhook-sa in namespace $TEKTON_NAMESPACE ..."
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+echo "Setting up tekton-triggers-createwebhook-sa in namespace $PIPELINE_NAMESPACE ..."
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: v1
 kind: ServiceAccount
 metadata:
   name: tekton-triggers-createwebhook-sa
+  labels:
+    managed-by: kubementat
 EOF
 
-echo "Binding tekton-triggers-createwebhook-sa to tekton-triggers-createwebhook-role in namespace $TEKTON_NAMESPACE ..."
-kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+echo "Binding tekton-triggers-createwebhook-sa to tekton-triggers-createwebhook-role in namespace $PIPELINE_NAMESPACE ..."
+kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
 metadata:
   name: tekton-triggers-createwebhook-rolebinding
+  labels:
+    managed-by: kubementat
 subjects:
   - kind: ServiceAccount
     name: tekton-triggers-createwebhook-sa
@@ -216,20 +201,22 @@ echo "Finished setting up roles for tekton triggers."
 
 
 echo "#########################"
-echo "Configuring webhook secrets in namespace $TEKTON_NAMESPACE ..."
+echo "Configuring webhook secrets in namespace $PIPELINE_NAMESPACE ..."
 
-echo "Configuring GITLAB_WEBHOOK_SECRET ..."
+echo "Configuring gitlab-trigger-webhook-secret from GITLAB_WEBHOOK_SECRET variable in namespace: $PIPELINE_NAMESPACE ..."
 set +e
-GITLAB_WEBHOOK_SECRET="$(jq -r '.GITLAB_WEBHOOK_SECRET' ../../platform_config/"${ENVIRONMENT}"/static.encrypted.json)"
+GITLAB_WEBHOOK_SECRET="$(jq -r '.GITLAB_WEBHOOK_SECRET' "../../platform_config/${ENVIRONMENT}/${TEAM}/static.encrypted.json")"
 set -e
 if [[ "$GITLAB_WEBHOOK_SECRET" != "" ]]; then
   echo "Found GITLAB_WEBHOOK_SECRET configuration. Configuring gitlab-trigger-webhook-secret on cluster"
   GITLAB_WEBHOOK_SECRET_BASE64=$(echo -n "${GITLAB_WEBHOOK_SECRET}" | base64)
-  kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+  kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
   name: gitlab-trigger-webhook-secret
+  labels:
+    managed-by: kubementat
 type: Opaque
 data:
   secretToken: |
@@ -237,18 +224,20 @@ data:
 EOF
 fi
 
-echo "Configuring GITHUB_WEBHOOK_SECRET ..."
+echo "Configuring github-trigger-webhook-secret from GITHUB_WEBHOOK_SECRET variable in namespace: $PIPELINE_NAMESPACE ..."
 set +e
-GITHUB_WEBHOOK_SECRET="$(jq -r '.GITHUB_WEBHOOK_SECRET' ../../platform_config/"${ENVIRONMENT}"/static.encrypted.json)"
+GITHUB_WEBHOOK_SECRET="$(jq -r '.GITHUB_WEBHOOK_SECRET' "../../platform_config/${ENVIRONMENT}/${TEAM}/static.encrypted.json")"
 set -e
 if [[ "$GITHUB_WEBHOOK_SECRET" != "" ]]; then
   echo "Found GITHUB_WEBHOOK_SECRET configuration. Configuring github-trigger-webhook-secret on cluster"
   GITHUB_WEBHOOK_SECRET_BASE64=$(echo -n "${GITHUB_WEBHOOK_SECRET}" | base64)
-  kubectl apply -n "$TEKTON_NAMESPACE" -f - <<EOF
+  kubectl apply -n "$PIPELINE_NAMESPACE" -f - <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
   name: github-trigger-webhook-secret
+  labels:
+    managed-by: kubementat
 type: Opaque
 data:
   secretToken: |
@@ -261,8 +250,8 @@ res="$(ls "../triggers/${TEAM}")"
 if [[ "$res" == ""  ]]; then
   echo "No triggers configured for team: ${TEAM}"
 else
-  for dir in ../triggers/$TEAM/*/ ; do
-    echo "Setting up triggers from: $dir ..."
-    kubectl apply -n "${TEKTON_NAMESPACE}" -f $dir
+  for dir in ../triggers/"$TEAM"/*/ ; do
+    echo "Setting up triggers from: $dir in namespace: $PIPELINE_NAMESPACE ..."
+    kubectl apply -n "${PIPELINE_NAMESPACE}" -f $dir
   done
 fi
